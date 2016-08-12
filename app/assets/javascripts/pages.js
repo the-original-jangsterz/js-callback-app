21236516880a04a4f9cde2987b78280671ef0287
Peter Jang <peter.x.jang@gmail.com>
2016-08-11 19:46:28 -0500

    Get API data using $.get

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -1,16 +1,3 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
-function changeColors() {
-  setTimeout(function() {
-    document.getElementById('first').style['color'] = 'red';
-    setTimeout(function() {
-      document.getElementById('second').style['color'] = 'blue';
-      setTimeout(function() {
-        document.getElementById('third').style['color'] = 'green';
-        setTimeout(function() {
-          document.getElementById('fourth').style['color'] = 'purple';
-        }, 1000);
-      }, 1000);
-    }, 1000);
-  }, 1000);
-}
+$.get('https://data.cityofchicago.org/resource/xzkq-xp2w.json?$limit=10', function(data) {
+  console.log(data);
+});
