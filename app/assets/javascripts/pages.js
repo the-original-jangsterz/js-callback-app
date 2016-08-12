e6f40213e5a646ac079501327bed33bc66b77ea5
Peter Jang <peter.x.jang@gmail.com>
2016-08-11 21:31:00 -0500

    Add filter buttons

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -1,66 +1,4 @@
 /* global $ */
-// var tempData = [
-//   {
-//     "job_titles": "WATER RATE TAKER",
-//     "employee_annual_salary": "90744.00",
-//     "name": "AARON,  ELVIA J",
-//     "department": "WATER MGMNT"
-//   },
-//   {
-//     "job_titles": "POLICE OFFICER",
-//     "employee_annual_salary": "84450.00",
-//     "name": "AARON,  JEFFERY M",
-//     "department": "POLICE"
-//   },
-//   {
-//     "job_titles": "POLICE OFFICER",
-//     "employee_annual_salary": "84450.00",
-//     "name": "AARON,  KARINA",
-//     "department": "POLICE"
-//   },
-//   {
-//     "job_titles": "CHIEF CONTRACT EXPEDITER",
-//     "employee_annual_salary": "89880.00",
-//     "name": "AARON,  KIMBERLEI R",
-//     "department": "GENERAL SERVICES"
-//   },
-//   {
-//     "job_titles": "CIVIL ENGINEER IV",
-//     "employee_annual_salary": "106836.00",
-//     "name": "ABAD JR,  VICENTE M",
-//     "department": "WATER MGMNT"
-//   },
-//   {
-//     "job_titles": "ASST TO THE ALDERMAN",
-//     "employee_annual_salary": "70764.00",
-//     "name": "ABARCA,  ANABEL",
-//     "department": "CITY COUNCIL"
-//   },
-//   {
-//     "job_titles": "GENERAL LABORER - DSS",
-//     "employee_annual_salary": "41849.60",
-//     "name": "ABARCA,  EMMANUEL",
-//     "department": "STREETS & SAN"
-//   },
-//   {
-//     "job_titles": "TRAFFIC CONTROL AIDE-HOURLY",
-//     "employee_annual_salary": "20051.20",
-//     "name": "ABASCAL,  REECE E",
-//     "department": "OEMC"
-//   },
-//   {
-//     "job_titles": "STAFF ASST TO THE ALDERMAN",
-//     "employee_annual_salary": "49452.00",
-//     "name": "ABBASI,  CHRISTOPHER",
-//     "department": "CITY COUNCIL"
-//   },
-//   {
-//     "job_titles": "ELECTRICAL MECHANIC",
-//     "employee_annual_salary": "93600.00",
-//     "name": "ABBATACOLA,  ROBERT J",
-//     "department": "AVIATION"
-//   }
-// ];
 
 function getData() {
   var divEmployees = document.getElementById('employees');
@@ -69,7 +7,7 @@ function getData() {
     var htmlString = "";
     for (var i = 0; i < data.length; i++) {
       var employee = data[i];
-      htmlString += '<div onclick="changeEmployee(this)">';
+      htmlString += '<div class="employee" onclick="changeEmployee(this)">';
       htmlString += '<h2>' + employee.name + '</h2>';
       htmlString += '<p>Title: ' + employee.title + '</p>';
       htmlString += '<p>Department: ' + employee.department + '</p>';
@@ -85,5 +23,25 @@ function changeEmployee(divEmployee) {
     divEmployee.style['color'] = 'black';
   } else {
     divEmployee.style['color'] = 'lightgray';
+  }
+}
+
+function showPolice() {
+  var employees = document.querySelectorAll('.employee');
+  for (var i = 0; i < employees.length; i++) {
+    var employee = employees[i];
+    if (employee.innerHTML.indexOf('POLICE') !== -1) {
+      employee.style['display'] = 'block';
+    } else {
+      employee.style['display'] = 'none';
+    }
+  }
+}
+
+function showAll() {
+  var employees = document.querySelectorAll('.employee');
+  for (var i = 0; i < employees.length; i++) {
+    var employee = employees[i];
+    employee.style['display'] = 'block';
   }
 }
