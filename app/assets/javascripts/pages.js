a04f17c22f5f403a5112e00ddefaa54e8e04a225
Peter Jang <peter.x.jang@gmail.com>
2016-08-11 20:30:54 -0500

    Render API data to DOM

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -1,3 +1,12 @@
-$.get('https://data.cityofchicago.org/resource/xzkq-xp2w.json?$limit=10', function(data) {
-  console.log(data);
-});
+/* global $ */
+
+function getData() {
+  var divEmployees = document.getElementById('employees');
+  divEmployees.innerHTML = "";
+  $.get('https://data.cityofchicago.org/resource/xzkq-xp2w.json?$limit=10', function(data) {
+    for (var i = 0; i < data.length; i++) {
+      var employee = data[i];
+      divEmployees.innerHTML += '<h2>' + employee.name + '</h2>';
+    }
+  });
+}
