e8a804cd96832bb8c788f1c539c5fe48ea308b1d
Peter Jang <peter.x.jang@gmail.com>
2016-08-11 20:33:19 -0500

    Add loading message

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -2,8 +2,9 @@
 
 function getData() {
   var divEmployees = document.getElementById('employees');
-  divEmployees.innerHTML = "";
+  divEmployees.innerHTML = "<p>Loading...</p>";
   $.get('https://data.cityofchicago.org/resource/xzkq-xp2w.json?$limit=10', function(data) {
+    divEmployees.innerHTML = "";
     for (var i = 0; i < data.length; i++) {
       var employee = data[i];
       divEmployees.innerHTML += '<h2>' + employee.name + '</h2>';
